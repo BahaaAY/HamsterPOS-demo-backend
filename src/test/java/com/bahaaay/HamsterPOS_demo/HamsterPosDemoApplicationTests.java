a7d125e90f987d91c89a8fdad7a8ac833a476d0d
BahaaAY <bahaa.abouyassine2@gmail.com>
2024-07-25 00:01:57 +0300

    Setting Up Spring Project

---
+++ src/test/java/com/bahaaay/HamsterPOS_demo/HamsterPosDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahaaay.HamsterPOS_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamsterPosDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
