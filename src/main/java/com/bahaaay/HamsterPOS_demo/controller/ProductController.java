ad52d4d14f2deb3c92eb3b4147bb62dabe4fd511
BahaaAY <bahaa.abouyassine2@gmail.com>
2024-07-25 04:30:31 +0300

    finished Products

--- src/main/java/com/bahaaay/HamsterPOS_demo/controller/ProductController.java
+++ src/main/java/com/bahaaay/HamsterPOS_demo/controller/ProductController.java
@@ -1,22 +1,56 @@
 package com.bahaaay.HamsterPOS_demo.controller;
 
 import com.bahaaay.HamsterPOS_demo.model.Product;
-import com.bahaaay.HamsterPOS_demo.repository.ProductRepository;
+import com.bahaaay.HamsterPOS_demo.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@Validated
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
+
     @Autowired
-    private ProductRepository productRepository;
+    private ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Product> addProduct(@Valid @RequestBody Product product) {
+        Product createdProduct = productService.addProduct(product);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
 
     @GetMapping
-    public List<Product> getAllProducts(){
-        return this.productRepository.findAll();
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return productService.getProductById(id)
+                .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @Valid @RequestBody Product product) {
+        Product updatedProduct = productService.updateProduct(id, product);
+        if (updatedProduct != null) {
+            return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
---
+++ src/main/java/com/bahaaay/HamsterPOS_demo/service/ProductService.java
@@ -0,0 +1,45 @@
+package com.bahaaay.HamsterPOS_demo.service;
+
+import com.bahaaay.HamsterPOS_demo.model.Product;
+import com.bahaaay.HamsterPOS_demo.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product updateProduct(Long id, Product updatedProduct) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setName(updatedProduct.getName());
+            product.setPrice(updatedProduct.getPrice());
+            product.setQuantity(updatedProduct.getQuantity());
+            return productRepository.save(product);
+        }
+        return null;
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
+
